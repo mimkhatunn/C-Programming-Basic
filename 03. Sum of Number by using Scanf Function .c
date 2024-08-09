195913d0797d3be46f7e76bd2d6968e0b4ccc02f
Mim khatun <157308121+mimkhatunn@users.noreply.github.com>
2024-08-10 01:54:02 +0600

    Create 03. Sum of Number by using Scanf Function .c

---
+++ 03. Sum of Number by using Scanf Function .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+float a, b, c;
+
+printf("Enter the first: ");
+scanf("%f", &a);
+
+printf("Enter the Second number: ");
+scanf("%f", &b);
+
+c=a+b;
+
+printf("sum = %.2f", c);
+
+return 0;
+}
