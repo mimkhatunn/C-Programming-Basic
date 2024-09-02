f09ee4802e2c640baacd4e457cb8680a86e63ff6
Mim khatun <157308121+mimkhatunn@users.noreply.github.com>
2024-09-02 23:18:58 +0600

    Create 05. If else if and else using c programming.c

---
+++ 05. If else if and else using c programming.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+int a;
+printf("Enter a number: ");
+scanf("%d",&a);
+
+if(a==1000){
+printf("Mim");
+}
+else if(a==2000) {
+
+printf("Athina");
+
+}
+else
+print("Not found");
+
+return 0;
+}
